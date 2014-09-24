512d1bfbdf090b116c64d5bd577dd0b875f93bb3
gilbo <gilbert@gilbertbernstein.com>
2014-09-24 16:39:30 -0700

    changed back to forward slash, now load by running load setup from node repl

---
+++ setup.js
@@ -0,0 +1,9 @@
+
+var Expr = require('./lib/expr');
+
+var t1 = Expr.parse('(/x -> y) ((/x -> x x) (/y -> y))');
+var t2 = Expr.parse('(/x -> (/y -> x y)) (/x -> (x y) z)');
+var t3 = Expr.parse('(/z -> (/y -> (/z -> z) z)) (/x -> y)');
+var t4 = Expr.parse('(/x -> f (x x)) (/x -> f (x x))');
+
+
